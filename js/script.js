507a87ed2bd342ff5dc653494090c7112340f700
ShiKirill <shiraevkirill@gmail.com>
2021-10-08 15:14:05 +0700

    Fixes

--- js/script.js
+++ js/script.js
@@ -5,44 +5,8 @@ const scrollDown = document.querySelector('.arrow-down');
 let scrollPoint = document.querySelector('.services-block').offsetTop;
 const subscriptionEmail = document.querySelector('.subscription-email');
 const subscriptionBtn = document.querySelector('.subscription-btn');
-
-const sliderWrapper = document.querySelector('.slider-block-wrapper');
-const sliderMain = document.querySelector('.slider-block-items');
-const slides = document.querySelectorAll('.slider-block-item');
-const dots = document.querySelectorAll('.dot');
-const paginationBlock = document.querySelector('.slider-block-pagination');
-let currentSlide = 0;
-
-const prevSlide = (elem, index, strClass) => {
-  if (elem[index].classList.contains('dot')) {
-    elem[index].classList.remove(strClass);
-  } else if (elem[index].classList.contains('.slider-block-item')) {
-    elem[index].classList.remove(strClass);
-  }
-};
-
-const nextSlide = (elem, index, strClass) => {
-  if (elem[index].classList.contains('dot')) {
-    elem[index].classList.add(strClass);
-  } else if (elem[index].classList.contains('.slider-block-item')) {
-    elem[index].classList.add(strClass);
-  }
-};
-
-paginationBlock.addEventListener('click', e => {
-  const target = e.target;
-
-  prevSlide(dots, currentSlide, 'active');
-  dots.forEach((item, index) => {
-    if (item === target) {
-      currentSlide = index;
-    }
-  });
-  const widthSlide = slides[0].clientWidth;
-  sliderMain.style.transform = `translateX(-${currentSlide * (widthSlide+30)}px)`;
-  nextSlide(dots, currentSlide, 'active');
-});
-
+const burgerBtn = document.querySelector('.menu-btn');
+const burgerMenu = document.querySelector('.menu-box');
 
 planChoiseContainer.addEventListener('click', e => {
   const target = e.target.closest('div');
@@ -60,6 +24,19 @@ window.addEventListener('scroll', () => {
   }
 });
 
+$(document).ready(function () {
+  jQuery('.slider').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    variableWidth: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+
+});
+
 scrollUpBtn.addEventListener('click', () => {
   document.querySelector('header').scrollIntoView({
     behavior: 'smooth',
@@ -74,13 +51,27 @@ scrollDown.addEventListener('click', () => {
   });
 });
 
-subscriptionEmail.addEventListener('input', (e)=>{
+subscriptionEmail.addEventListener('input', (e) => {
   const target = e.target;
-  if (target.value === ""){
+  if (target.value === "") {
     subscriptionBtn.setAttribute("data-bs-toggle", "modal");
     subscriptionBtn.setAttribute("data-bs-target", "#errorModal")
   } else {
     subscriptionBtn.setAttribute("data-bs-toggle", "modal");
     subscriptionBtn.setAttribute("data-bs-target", "#exampleModal");
   }
-})
+});
+
+window.addEventListener('click', e => {
+  const target = e.target;
+  if (burgerMenu.style.visibility == 'visible') {
+    if (target !== burgerMenu) {
+      burgerMenu.style.visibility = 'hidden';
+      burgerMenu.style.opacity = 0;
+    }
+  }
+  if (target.closest('.menu-btn')) {
+    burgerMenu.style.visibility = 'visible';
+    burgerMenu.style.opacity = 1;
+  }
+});
